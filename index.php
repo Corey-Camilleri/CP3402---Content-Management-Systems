f4a5a0e4ff197c7f4c8f73f6921128b3a8df965e
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2026-02-25 12:07:03 +1000

    Initial theme

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+get_header();
+?>
+<main style="padding:2rem;">
+  <h1>Hello from my custom theme!</h1>
+  <p>If you can see this, your theme is working.</p>
+</main>
+<?php
+get_footer();
+?>
