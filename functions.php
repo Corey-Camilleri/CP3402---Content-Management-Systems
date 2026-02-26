48f37dc509745137b1d90d4fb036853290a7b655
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2026-02-26 11:22:38 +1000

    Refactor enqueue styles function for child theme and update version number in style.css

--- functions.php
+++ functions.php
@@ -1,9 +1,13 @@
 <?php
-function my_child_theme_enqueue_styles() {
-    wp_enqueue_style(
-        'parent-style',
-        get_template_directory_uri() . '/style.css'
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    $parent_style = 'Twenty Twenty-Five'; // This is the handle used by the parent theme's stylesheet
+    
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ), // Make the parent style a dependency
+        wp_get_theme()->get('Version') // Use child theme version for cache busting
     );
 }
-add_action('wp_enqueue_scripts', 'my_child_theme_enqueue_styles');
 ?>
