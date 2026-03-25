788c188b78f0d68b9df5d3637bd250ed6bfa6dce
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2026-03-25 13:43:11 +1000

    Remove HTML structure from index.php and update style.css with footer styles

--- index.php
+++ index.php
@@ -1,13 +1,1 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Home</title>
-    <link href="styles/mainstyles.css" rel="stylesheet" type="text/css" media="screen">
-</head>
-
-<body>
-    <h1>Welcome to my WordPress Theme</h1>
-    <p>This is the index.php file of my custom WordPress theme.</p>
-</body>
-</html>
+<?php
