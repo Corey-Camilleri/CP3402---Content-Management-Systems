fe48a527707beceae26dd629a8ba07a8c42f805e
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2026-03-25 13:24:06 +1000

    Add HTML structure to index.php for WordPress theme

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php
-// Silence is golden.
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link href="styles/mainstyles.css" rel="stylesheet" type="text/css" media="screen">
+</head>
+
+<body>
+    <h1>Welcome to my WordPress Theme</h1>
+    <p>This is the index.php file of my custom WordPress theme.</p>
+</body>
+</html>
